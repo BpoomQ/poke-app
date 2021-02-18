0458641801763c7e2ae6ecfdf79549a505b029df
Alejandro Tarazona <luisalejo0602@gmail.com>
2021-02-18 00:11:30 -0500

    update functions

--- src/components/liked-context/LikedContext.js
+++ src/components/liked-context/LikedContext.js
@@ -5,14 +5,14 @@ export const LikedContext = createContext();
 export const LikedProvider = ({ children }) => {
   const [likedPokemons, setLikedPokemons] = useState([]);
 
-  const addLiked = (pokemonName) =>
-    setLikedPokemons([...likedPokemons, pokemonName]);
+  const addLiked = (pokemonName, url) =>
+    setLikedPokemons([...likedPokemons, { name: pokemonName, url }]);
 
   const removeLiked = (pokemonName) =>
-    setLikedPokemons(likedPokemons.filter((name) => name !== pokemonName));
+    setLikedPokemons(likedPokemons.filter(({ name }) => name !== pokemonName));
 
   const isLiked = (pokemonName) =>
-    !!likedPokemons.find((name) => name === pokemonName);
+    !!likedPokemons.find(({ name }) => name === pokemonName);
 
   return (
     <LikedContext.Provider
